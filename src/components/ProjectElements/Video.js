0e0891a3bb5744bdb72459e804c4333e726415f4
Alden Brown <87581687+AldenB0@users.noreply.github.com>
2023-08-17 19:28:50 -0500

    added dangerouslySetInnerHTML to the other videos

--- src/components/ProjectElements/Video.js
+++ src/components/ProjectElements/Video.js
@@ -1,16 +1,21 @@
 import React from "react";
 
 const Video = ({ src, divStyle, videoStyle }) => {
+  const videoHTML = `
+    <video autoplay loop muted playsinline class="${videoStyle}">
+      <source src="${src}" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+  `;
+
   // Combine the existing styles with the additional styles
   const combinedDivStyle = `rounded-3xl overflow-hidden flex justify-center ${divStyle}`;
 
   return (
-    <div className={combinedDivStyle}>
-      <video autoPlay loop muted playsInline className={videoStyle}>
-        <source src={src} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <div
+      className={combinedDivStyle}
+      dangerouslySetInnerHTML={{ __html: videoHTML }}
+    />
   );
 };
 
